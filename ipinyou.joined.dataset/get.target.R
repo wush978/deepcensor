26620534bfcbe9f27b9e09442e88619204dce45d
Wush Wu <wush978@gmail.com>
2018-06-15 13:54:10 +0000

    use season join

--- ipinyou.joined.dataset/get.target.R
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env Rscript
-f <- file("stdin")
-open(f)
-while(length(line <- readLines(f, n = 1)) > 0) {
-  tokens <- strsplit(line, ".", fixed = TRUE)[[1]]
-  cat(grep("^\\d+$", tokens, value = TRUE))
-  cat(".Rds\n")
-}
